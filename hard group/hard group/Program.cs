85f59367f4183b7d10ee1601c2130d38e75d49a2
shinx-x <lanamonthorova@gmail.com>
2024-11-27 15:22:22 +0300

    добавила стартовый проект

---
+++ hard group/hard group/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hard_group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
